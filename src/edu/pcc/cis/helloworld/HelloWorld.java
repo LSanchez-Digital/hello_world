a729bb24912b9964e0b41670a362a450bec3d822
LSanchez-Digital <LSanchez.digital@gmail.com>
2017-02-07 13:55:20 -0800

    added today's date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Luis on 2/5/2017.
  */
@@ -7,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello, World! I'm Luis");
+        System.out.println(new Date());
     }
 }
