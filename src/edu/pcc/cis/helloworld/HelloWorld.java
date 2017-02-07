24172dce64ed7e199d6b60c2c4a518df6c5491a4
LSanchez-Digital <LSanchez.digital@gmail.com>
2017-02-07 13:53:05 -0800

    added main method to print hello

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -3,5 +3,9 @@ package edu.pcc.cis.helloworld;
 /**
  * Created by Luis on 2/5/2017.
  */
+
 public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello, World! I'm Luis");
+    }
 }
